d42ff24db2bf03c839ad54fc7eed0abc5001edf6
EVEREST <61843974+everest-sp@users.noreply.github.com>
2022-10-29 19:55:36 +0500

    29102022-1955

---
+++ js/cursor.js
@@ -0,0 +1,31 @@
+const cursor = document.getElementById("cursor");
+document.addEventListener("mousemove", (e) => {
+	let leftPosition = e.pageX + 1;
+	let topPosition = e.pageY - window.scrollY + 1;
+
+	cursor.style.left = leftPosition + "px";
+	cursor.style.top = topPosition + "px";
+})
+document.addEventListener("mouseover", (e) => {
+	let leftPosition = e.pageX + 1;
+	let topPosition = e.pageY - window.scrollY + 1;
+
+	cursor.style.left = leftPosition + "px";
+	cursor.style.top = topPosition + "px";
+
+	const target = e.path[0]
+	if (target && target.tagName === 'A' || target.className.includes('btn-ripple') === true) {
+		cursor.classList.add('cursor-hover');
+	}
+})
+document.addEventListener('mouseout', function(){
+	cursor.classList.remove('cursor-click');
+	cursor.classList.remove('cursor-hover');
+});
+
+document.addEventListener('mousedown', function(){
+	cursor.classList.add('cursor-click');
+});
+document.addEventListener('mouseup', function(){
+	cursor.classList.remove('cursor-click');
+});
---
+++ js/scripts.js
@@ -0,0 +1,152 @@
+
+$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 500);
+		}
+		hidePreloader();
+
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+    // closes the responsive menu on menu item click
+    $(".navbar-nav li a").on("click", function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+    /* Rotating Text - Morphtext */
+	$("#js-rotating").Morphext({
+		// The [in] animation type. Refer to Animate.css for a list of available animations.
+		animation: "fadeIn",
+		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+		separator: ",",
+		// The delay between the changing of each phrase in milliseconds.
+		speed: 2000,
+		complete: function () {
+			// Called after the entrance animation is executed.
+		}
+    });
+    
+
+    
+    /* Video Lightbox - Magnific Popup */
+    $('.popup-youtube, .popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/', 
+                    id: function(url) {        
+                        var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                        if ( !m || !m[1] ) return null;
+                        return m[1];
+                    },
+                    src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+                },
+                vimeo: {
+                    index: 'vimeo.com/', 
+                    id: function(url) {        
+                        var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+                        if ( !m || !m[5] ) return null;
+                        return m[5];
+                    },
+                    src: 'https://player.vimeo.com/video/%id%?autoplay=1'
+                }
+            }
+        }
+    });
+
+
+    /* Lightbox - Magnific Popup */
+	$('.popup-with-move-anim').magnificPopup({
+		type: 'inline',
+		fixedContentPos: false, /* keep it false to avoid html tag shift with margin-right: 17px */
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
+    
+
+    /* Counter - CountTo */
+	var a = 0;
+	$(window).scroll(function() {
+		if ($('#counter').length) { // checking if CountTo section exists in the page, if not it will not run the script and avoid errors	
+			var oTop = $('#counter').offset().top - window.innerHeight;
+			if (a == 0 && $(window).scrollTop() > oTop) {
+			$('.counter-value').each(function() {
+				var $this = $(this),
+				countTo = $this.attr('data-count');
+				$({
+				countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				},
+				{
+					duration: 2000,
+					easing: 'swing',
+					step: function() {
+					$this.text(Math.floor(this.countNum));
+					},
+					complete: function() {
+					$this.text(this.countNum);
+					//alert('finished');
+					}
+				});
+			});
+			a = 1;
+			}
+		}
+    });
+
+
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+    });
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
