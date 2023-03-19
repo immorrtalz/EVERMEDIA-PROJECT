371657c1238179242d28988f215b4d56cc3452b2
EVEREST <61843974+evermedia-project@users.noreply.github.com>
2023-03-19 17:29:47 +0500

    19032023-1728

--- js/cursor.js
+++ js/cursor.js
@@ -1,4 +1,5 @@
 const cursor = document.getElementById("cursor");
+
 document.addEventListener("mousemove", (e) => {
 	let leftPosition = e.pageX + 1;
 	let topPosition = e.pageY - window.scrollY + 1;
@@ -6,6 +7,7 @@ document.addEventListener("mousemove", (e) => {
 	cursor.style.left = leftPosition + "px";
 	cursor.style.top = topPosition + "px";
 })
+
 document.addEventListener("mouseover", (e) => {
 	let leftPosition = e.pageX + 1;
 	let topPosition = e.pageY - window.scrollY + 1;
@@ -17,17 +19,9 @@ document.addEventListener("mouseover", (e) => {
 	if (target.tagName === 'A' || target.className.includes('btn-ripple') === true)
 	{
 		cursor.classList.add('cursor-hover');
-
-		/*var elm = $("<span/>").appendTo(target).css({
-			position: "absolute",
-			top: 0,
-			left: 0,
-			width: 100 +"%",
-			height: 100 + "%",
-			background: "transparent",
-		});*/
 	}
 })
+
 document.addEventListener('mouseout', function(){
 	cursor.classList.remove('cursor-click');
 	cursor.classList.remove('cursor-hover');
@@ -36,6 +30,7 @@ document.addEventListener('mouseout', function(){
 document.addEventListener('mousedown', function(){
 	cursor.classList.add('cursor-click');
 });
+
 document.addEventListener('mouseup', function(){
 	cursor.classList.remove('cursor-click');
 });
---
+++ js/setfooter.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", (event) => {
+	const footer = document.querySelector('footer');
+	const footercont = $('<div class="container">').appendTo(footer);
+
+	const footer1 = $('<div class="container-footer footer-1"></div>').appendTo(footercont);
+	const footer2 = $('<div class="container-footer footer-2"></div>').appendTo(footercont);
+	const footer3 = $('<div class="container-footer footer-3"></div>').appendTo(footercont);
+
+	$("<h2>О НАС</h2>").appendTo(footer1);
+	$("<p>EVERMEDIA PROJECT - медиапроект, специализирующийся в области разработки видеоигр, графического дизайна и иного мультимедиа контента</p>").appendTo(footer1);
+
+	$("<h2>ССЫЛКИ</h2>").appendTo(footer2);
+	var elm = $('<div class="flex-container"></div>').appendTo(footer2);
+	$('<a class="extra-link-icon" href="https://www.youtube.com/c/EVERESTSTUDIOProject"><img src="images/socials/yt.png"></a>').appendTo(elm);
+	$('<a class="extra-link-icon" href="https://discord.gg/GbzYVdF"><img src="images/socials/ds.png"></a>').appendTo(elm);
+	$('<a class="extra-link-icon" href="https://t.me/everest_studio_project"><img src="images/socials/tg_channel.png"></a>').appendTo(elm);
+	$('<a class="extra-link-icon" href="https://t.me/everest_studio_project_cm"><img src="images/socials/tg_chat.png"></a>').appendTo(elm);
+	$('<a class="extra-link-icon" href="https://vk.com/everest_studio_project"><img src="images/socials/vk.png"></a>').appendTo(elm);
+
+	var bottomstr1 = '<p>Copyright © EVERMEDIA PROJECT, 2019-';
+	var bottomstr2 = new Date().getFullYear();
+	var bottomstr3 = '. All rights reserved.</p><a href="mailto:evereststudioproject@bk.ru" class="blue"> evereststudioproject@bk.ru</a>';
+	var bottomstr = bottomstr1 + bottomstr2 + bottomstr3;
+
+	$(bottomstr).appendTo(footer3);
+});
