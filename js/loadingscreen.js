7ec0c4246ca50a49813931a9837efa8ca3a2e919
EVEREST <61843974+evermedia-project@users.noreply.github.com>
2023-03-29 12:51:26 +0500

    26032023-1251

--- js/loadingscreen.js
+++ js/loadingscreen.js
@@ -1,10 +1,14 @@
+$("body").css("overflow", "hidden");
+
 $(window).on('load', function() {
 	var preloaderFadeOutTime = 500;
 	function hidePreloader()
 	{
 		var preloader = $('.loading-container');
+		preloader.css("top", window.scrollY + 1);
 		setTimeout(function() {
 			preloader.fadeOut(preloaderFadeOutTime);
+			$("body").css("overflow", "auto");
 		}, 750);
 	}
 	hidePreloader();
