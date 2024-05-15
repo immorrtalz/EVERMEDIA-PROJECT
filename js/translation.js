be240dc774e6eaa02c129af8651f8c1b4990bbd3
immorrtalz <61843974+immorrtalz@users.noreply.github.com>
2024-05-15 23:16:32 +0500

    15052024-2316

--- js/translation.js
+++ js/translation.js
@@ -69,7 +69,7 @@ const translationsEN = [
 	"Copyrights and other aspects",
 	"ABOUT THE GAME",
 	"Creation history, gameplay, etc.",
-	"Development of the game began on January 23, 2022, on a quiet winter evening. By the summer of that year, several alpha builds had been released, and the first testers appeared.\u2028\u2028By the end of the year, the game went into beta, and the speed of development increased greatly, more and more attention was paid to the comfort of the game process.\u2028\u2028The last beta version was released in May 2023, and on June 24 of the same year The game was released on four platforms."//currently 60
+	"Development of the game began on January 23, 2022, on a quiet winter evening. By the summer of that year, several alpha builds had been released, and the first testers appeared.\n\nBy the end of the year, the game went into beta, and the speed of development increased greatly, more and more attention was paid to the comfort of the game process.\n\nThe last beta version was released in May 2023, and on June 24 of the same year The game was released on four platforms."//currently 60
 ];
 
 const translationsRU = [
