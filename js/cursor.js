7ca93d091c40efd6d9031f9b983f894487b1afe6
immorrtalz <61843974+immorrtalz@users.noreply.github.com>
2024-10-13 12:58:24 +0500

    13102024-1258

--- js/cursor.js
+++ js/cursor.js
@@ -21,19 +21,6 @@ else
 
 	raf = window.requestAnimationFrame(animFrame);
 
-	function animFrame()
-	{
-		cursor.style.left = lerp(cursor.style.left.replace('px', ''), inputX, 0.1) + "px";
-		cursor.style.top = lerp(cursor.style.top.replace('px', ''), inputY, 0.1) + "px";
-
-		cursorGlow.style.left = lerp(cursorGlow.style.left.replace('px', ''), inputX, 0.9) + "px";
-		cursorGlow.style.top = lerp(cursorGlow.style.top.replace('px', ''), inputY, 0.9) + "px";
-
-		if (currentMouseTarget != 'null') cursorGlow.style.opacity = '0.5';
-		else cursorGlow.style.opacity = '0';
-
-		raf = window.requestAnimationFrame(animFrame);
-	}
 	document.addEventListener("mouseover", (e) =>
 	{
 		if (e.target.tagName === 'A' || e.target.className.includes('btn-ripple'))
@@ -52,6 +39,20 @@ else
 
 	document.addEventListener('mousedown', () => { cursor.classList.add('cursor-click'); });
 	document.addEventListener('mouseup', () => { cursor.classList.remove('cursor-click'); });
+}
+
+function animFrame()
+{
+	cursor.style.left = lerp(cursor.style.left.replace('px', ''), inputX, 0.1) + "px";
+	cursor.style.top = lerp(cursor.style.top.replace('px', ''), inputY, 0.1) + "px";
+
+	cursorGlow.style.left = lerp(cursorGlow.style.left.replace('px', ''), inputX, 0.9) + "px";
+	cursorGlow.style.top = lerp(cursorGlow.style.top.replace('px', ''), inputY, 0.9) + "px";
+
+	if (currentMouseTarget != 'null') cursorGlow.style.opacity = '0.5';
+	else cursorGlow.style.opacity = '0';
+
+	raf = window.requestAnimationFrame(animFrame);
 }
 
 function lerp(start, end, t) { return start * (1 - t) + end * t; }
